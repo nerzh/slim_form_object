a93562dd620a7b523fde94c3fe7599abd1dcd7aa
woodcrust <roboucrop@gmail.com>
2016-06-08 23:22:13 +0300

    fix snake for engine with ::

--- lib/slim_form_object/helpers.rb
+++ lib/slim_form_object/helpers.rb
@@ -1,5 +1,6 @@
 module HelperMethods
   def snake(string)
-    string.gsub(/((\w)([A-Z]))/,'\2_\3').downcase
+    string.gsub!(/((\w)([A-Z]))/,'\2_\3')
+    string.gsub(/::/,'_').downcase
   end
 end
--- lib/slim_form_object/version.rb
+++ lib/slim_form_object/version.rb
@@ -1,3 +1,3 @@
 module SlimFormObject
-  VERSION = "0.5.15"
+  VERSION = "0.5.16"
 end
