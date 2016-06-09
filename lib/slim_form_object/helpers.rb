e85c09cbe2d487a2a2bfccc6f55b101883336f48
woodcrust <roboucrop@gmail.com>
2016-06-09 11:31:59 +0300

    fix class name of model in engine that without 'name_engine::'

--- lib/slim_form_object/helpers.rb
+++ lib/slim_form_object/helpers.rb
@@ -1,6 +1,11 @@
 module HelperMethods
   def snake(string)
     string.gsub!(/((\w)([A-Z]))/,'\2_\3')
-    string.gsub(/::/,'_').downcase
+    class_name_if_module(string.downcase)
+  end
+
+  def class_name_if_module(string)
+    return $1 if string =~ /^.+::(.+)$/
+    string
   end
 end
